c140f13455198de5c7b043d5a73b25cc4bc3dfd0
BlisS <fixtergeek@gmail.com>
2023-10-08 06:50:29 -0600

    netlify adapter

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,15 @@
+const baseConfig =
+  process.env.NODE_ENV === "production"
+    ? {
+        server: "./server.js",
+        serverBuildPath: ".netlify/functions-internal/server.js",
+      }
+    : undefined;
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
+  ...baseConfig,
   ignoredRouteFiles: ["**/.*"],
+  // future: {},
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
